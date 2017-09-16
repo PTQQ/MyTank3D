c00a611ee9d4a4a2b65c53bb76e763f36c9f827d
PTQQ <397601312@qq.com>
2017-09-16 19:30:44 +0800

    这是一个空项目

---
+++ MyTank3D/Source/MyTank3D/MyTank3DGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyTank3DGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTANK3D_API AMyTank3DGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
